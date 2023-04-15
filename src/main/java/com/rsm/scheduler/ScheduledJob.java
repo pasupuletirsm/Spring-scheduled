27677b34e8da6c52cebf575d9b9db85896b6a157
Mani <pmaninvr@gmail.com>
2023-04-15 23:21:37 +0530

    initial commit

---
+++ src/main/java/com/rsm/scheduler/ScheduledJob.java
@@ -0,0 +1,30 @@
+package com.rsm.scheduler;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledJob {
+	
+	Logger log = LoggerFactory.getLogger(ScheduledJob.class);
+	
+	@Scheduled(fixedRate = 5000)
+	public void scheduledFixedRate() throws InterruptedException {
+		log.info("Excuted scheduledFixedRate every 5 seconds", new Date());
+	}
+	
+	@Scheduled(fixedDelayString  = "5000")
+	public void scheduledFixedDelay() throws InterruptedException {
+		log.info("Excuted scheduledFixedDelay every 5 seconds", new Date());
+	}
+	
+	@Scheduled(cron = "${scheduling.job.cron}")
+	public void scheduledCron() throws InterruptedException {
+		log.info("Excuted scheduledCron every 5 seconds", new Date());
+	}
+
+}
---
+++ src/test/java/com/rsm/SpringScheduledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScheduledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
