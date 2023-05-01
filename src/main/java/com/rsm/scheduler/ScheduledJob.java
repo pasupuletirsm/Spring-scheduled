8a55eb66dc03bd22effa77fadf9d52c5dd03e6e1
Mani <pmaninvr@gmail.com>
2023-05-01 16:00:19 +0530

    added all scheduler methods

--- src/main/java/com/rsm/scheduler/ScheduledJob.java
+++ src/main/java/com/rsm/scheduler/ScheduledJob.java
@@ -1,5 +1,6 @@
 package com.rsm.scheduler;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -10,22 +11,48 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledJob {
-	
+
 	Logger log = LoggerFactory.getLogger(ScheduledJob.class);
-	
+
 	@Scheduled(fixedRate = 5000)
-	public void scheduledFixedRate() throws InterruptedException {
-		log.info("Excuted scheduledFixedRate every 5 seconds", new Date());
-	}
-	
-	@Scheduled(fixedDelay  = 5, timeUnit = TimeUnit.SECONDS)
-	public void scheduledFixedDelay() throws InterruptedException {
-		log.info("Excuted scheduledFixedDelay every 5 seconds", new Date());
-	}
-	
-	@Scheduled(cron = "${scheduling.job.cron}")
-	public void scheduledCron() throws InterruptedException {
-		log.info("Excuted scheduledCron every 5 seconds", new Date());
+	public void scheduledFixedRate() {
+		log.info("Excuted scheduledFixedRate every 5 seconds {}", new Date());
 	}
 
+	@Scheduled(fixedDelay  = 5, timeUnit = TimeUnit.SECONDS)
+	public void scheduledFixedDelay() {
+		log.info("Excuted scheduledFixedDelay every 5 seconds {}", new Date());
+	}
+
+	@Scheduled(cron = "${scheduling.job.cron}")
+	public void scheduledCron() {
+		log.info("Excuted scheduledCron every 5 seconds {}", new Date());
+	}
+
+	@Scheduled(initialDelay = 2, fixedRate = 5,  timeUnit = TimeUnit.SECONDS)
+	public void scheduledInitialDelay() {
+		log.info("Excuted scheduledInitialDelay every 5 seconds {}", new Date());
+	}
+
+	@Scheduled(fixedRate = 3000)
+	public void doSomething() {
+		log.info("Scheduled job is running with thread: " + Thread.currentThread().getName() + " at time: "
+				+ LocalDateTime.now());
+		try {
+			Thread.sleep(5000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Scheduled(fixedRate = 3000)
+	public void doSomethingElse() {
+	    System.out.println("Other scheduled job is running with thread: " + Thread.currentThread().getName() +
+	            " at time: " + LocalDateTime.now());
+	    try {
+	        Thread.sleep(5000L);
+	    } catch (InterruptedException e) {
+	        e.printStackTrace();
+	    }
+	}
 }
