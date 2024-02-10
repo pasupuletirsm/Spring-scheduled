0cb8c13a7d58235a4d93df711692c36002ae2d2f
pasupuletirsm <pmaninvr@gmail.com>
2024-02-10 19:54:32 +0530

    cherry picked two commits

--- src/main/java/com/rsm/config/AppConfig.java
+++ src/main/java/com/rsm/config/AppConfig.java
@@ -3,15 +3,18 @@ package com.rsm.config;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+
 @Configuration
 @EnableScheduling
 public class AppConfig implements SchedulingConfigurer {
+	org.slf4j.Logger log = LoggerFactory.getLogger(getClass());
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
@@ -20,6 +23,7 @@ public class AppConfig implements SchedulingConfigurer {
 
     @Bean(destroyMethod="shutdown")
     public Executor taskExecutor() {
+    	System.out.println("Added Print message");
         return Executors.newScheduledThreadPool(3);
     }
 }
